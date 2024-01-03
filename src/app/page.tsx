6d929faa3d72ba4c0a67ca1079d140175bdd13dc
aka <aaronkaltman@gmail.com>
2024-01-03 14:26:23 -0500

    do it dog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 import InputAndList from "./_components/InputAndList";
-import prisma from "@/lib/db/prisma";
 import ListContainer from "./_components/_components_2/ListContainer";
 
 export default async function Home() {
-  const allComments = await prisma.corpComment.findMany({
-    where: {
-      comment: "hi",
-    },
-  });
   return (
     <main>
       <InputAndList />
       <ListContainer />
-      {JSON.stringify(allComments)}
     </main>
   );
 }
