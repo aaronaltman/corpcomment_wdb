d6c0a18e32e17f8d91fca33ab422a3a856d593e6
aka <aaronkaltman@gmail.com>
2024-01-04 18:55:59 -0500

    ok we tied in and working

--- src/app/_components/_components_2/ListComments.tsx
+++ src/app/_components/_components_2/ListComments.tsx
@@ -11,20 +11,9 @@ export default async function ListComments() {
     <div className="flex flex-col justify-center items-center mt-10 py-10 bg-slate-800 mb-10 shadow-xl rounded-lg">
       <h1 className="text-white text-4xl py-2 mb-6">Corporate Comments</h1>
       <ul className="">
-        {/* {allComments.map((comment) => ('<li>' + comment.comment + '</li>'))} */}
-        s
-        <li className="border border-slate-500/10">
-          {JSON.stringify(allComments)}
-        </li>
-        <li className="border border-slate-500/10">
-          <SingleListItem />
-        </li>
-        <li className="border border-slate-500/10">
-          <SingleListItem />
-        </li>
-        <li className="border border-slate-500/10">
-          <SingleListItem />
-        </li>
+        {allComments.map((corpComment) => (
+          <SingleListItem key={corpComment.id} corpComment={corpComment} />
+        ))}
       </ul>
     </div>
   );
--- src/app/_components/_components_2/SingleListItem.tsx
+++ src/app/_components/_components_2/SingleListItem.tsx
@@ -1,22 +1,31 @@
+"use server";
+
+import { corpComment as c } from "@prisma/client";
 import { ArrowUpIcon } from "lucide-react";
 import React from "react";
+import prisma from "@/lib/db/prisma";
 
-export default function SingleListItem() {
+type SingleListItemProps = {
+  corpComment: c;
+  key: string;
+};
+
+export default async function SingleListItem({
+  corpComment,
+  key,
+}: SingleListItemProps) {
+  const allComments = await prisma.corpComment.findMany();
   return (
     <>
-      <div className="flex flex-row bg-slate-100 gap-10 py-5 px-10 max-w-4xl rounded-md hover:scale-105 transition-all hover:my-2">
+      <div className="flex flex-row bg-slate-100 gap-12 py-5 px-10 max-w-4xl rounded-md hover:scale-105 transition-all hover:my-2">
         <div className="flex flex-col justify-center items-center gap-2">
           <p>423</p>
           <ArrowUpIcon size={20} />
         </div>
         <div>N</div>
         <div>
-          <div>Nike</div>
-          <div>
-            I really Liked Nike a lot. The store was friendly and the people
-            there were reallynice. I just wish they had that one shoe Ive been
-            looking for.
-          </div>
+          <div>{corpComment?.companyName}</div>
+          <div>{corpComment?.comment}</div>
         </div>
       </div>
     </>
